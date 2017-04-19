c50183391c0ad232eb7eb49afe49a63ba1e00c2b
fayezosaadi <fayezosaadi@gmail.com>
2017-04-19 05:36:29 +0000

    template strings-refactor

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -6,32 +6,31 @@
  /* globals $, moment */
 $(function onReady() {
   const $tweets = $('#tweets');
-
   const data = [
-    {
-      user: {
-        name: 'Newton',
-        avatars: {
-          'small': 'https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png',
-          'regular': 'https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png',
-          'large': 'https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png',
-        },
-        'handle': '@SirIsaac',
+  {
+    user: {
+      name: 'Newton',
+      avatars: {
+        'small': 'https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png',
+        'regular': 'https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png',
+        'large': 'https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png',
       },
-      'content': {
-        'text': 'If I have seen further it is by standing on the shoulders of giants',
-      },
-      'created_at': 1461116232227,
+      'handle': '@SirIsaac',
     },
-    {
-      'user': {
-        'name': 'Descartes',
-        'avatars': {
-          'small':   'https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png',
-          'regular': 'https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png',
-          'large':   'https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png',
-        },
-        'handle': '@rd' },
+    'content': {
+      'text': 'If I have seen further it is by standing on the shoulders of giants',
+    },
+    'created_at': 1461116232227,
+  },
+  {
+    'user': {
+      'name': 'Descartes',
+      'avatars': {
+        'small':   'https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png',
+        'regular': 'https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png',
+        'large':   'https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png',
+      },
+      'handle': '@rd' },
       'content': {
         'text': 'Je pense , donc je suis',
       },
@@ -52,11 +51,10 @@ $(function onReady() {
       },
       'created_at': 1492571528528,
     }
-  ];
+    ];
 
-
-  function template(tweet) {
-    return `<article class="tweet-container">
+    function template(tweet) {
+      return `<article class="tweet-container">
           <header>
             <img class="tweetlogo" src="${tweet.user.avatars.small}">
             <span class = "fullusername">${tweet.user.name}</span>
@@ -72,34 +70,11 @@ $(function onReady() {
               </div>
           </footer>
         </article>`;
-  }
+      }
 
   function enhanceWithTimeAgo(tweet) {
     return Object.assign({}, tweet, { created_at: moment(tweet.created_at).fromNow() });
   }
 
   $tweets.append(data.map(enhanceWithTimeAgo).map(template));
-
-// function createTweetElement(tweet) {
-//   var $tweet = $('<article>').addClass('tweet-container');
-//   var $header = $('<header>');
-//   var $img = $('<img>').addClass('tweetlogo').attr('src', tweet.small);
-//   var $span1 = $('<span>').addClass('fullusername');
-//   var $span2 = $('<span>').addClass('shortusername');
-//   var $span3 = $('<span>');
-//   var footer = $('<footer>');
-
-//   $('.tweets-container').append($tweet);
-//   $('.tweet-container').append($header);
-//   $($header).append($img);
-//   $($header).append($span1);
-//   $($header).append($span2);
-//   $('.tweet-container').append($span3);
-
-
-// }
-
-// var $tweet = createTweetElement(tweetData);
-//   console.log($tweet);
-// });
 });
